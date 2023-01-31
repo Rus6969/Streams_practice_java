e5713eae49e23b112f45adb62d3e317351760121
Rus6969 <russam4515@gmail.com>
2023-01-31 13:31:18 -0500

    Optional class is added

--- src/main/java/optionals/optionalExplained.java
+++ src/main/java/optionals/optionalExplained.java
@@ -9,17 +9,28 @@ public class optionalExplained {
   The class was introduced in the java.util package to remove the need for multiple null checks to protect against
   the dreaded NullPointerExceptions during run-time.
    */
-
-
         Optional<Object> empty = Optional.empty();// will return empty
         // uncomment me for nullable tests
-       // Optional<String> hello1 = Optional.ofNullable("Hello");
-        Optional<String> hello1 = Optional.ofNullable(null);
+        Optional<String> hello1 = Optional.ofNullable("Hello");
+       // Optional<String> hello1 = Optional.ofNullable(null);
         System.out.println("Is_Present*/*/*/*/*/*/*/*/*/*/");
         System.out.println(hello1.isPresent());
         System.out.println("/*/*/*/*/*Or_Else/*/*/*/*/*/");
         String inCaseHelloNotPresent = hello1.orElse("Bye! ");
         System.out.println(inCaseHelloNotPresent);
+
+        // using streams and lambda
+        String OrElse= hello1
+                .map(String::toUpperCase)
+                // or else is supplier we can add any logic to give as stadard else in if else statement
+                .orElseGet(()->{
+                            String hello = "Hello";
+                            return hello;
+                        }
+                );
+
+
+
 
 
     }
